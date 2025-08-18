99f43282eae7c579487a26d55b4753a046d02f30
Maruthigitzool <maruthi@zool.in>
2025-08-18 12:10:45 +0530

    chore: initial commit of theme v1.0.0

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<header class="site-header">
+    <div class="container">
+        <!-- <h1 class="site-title">
+            <a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+        </h1>
+        <p class="site-description"><?php bloginfo('description'); ?></p> -->
+    </div>
+</header>
